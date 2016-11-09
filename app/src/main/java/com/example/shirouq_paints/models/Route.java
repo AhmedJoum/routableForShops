b35be98dc39052a4556d61c724cac73da60a1b80
Carsiow <ahmed.moniim@gmail.com>
2016-11-09 10:23:03 +0300

    starting algorithem logic

---
+++ app/src/main/java/com/example/shirouq_paints/models/Route.java
@@ -0,0 +1,51 @@
+package com.example.shirouq_paints.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Carsiow on 11/8/16.
+ */
+
+public class Route implements Serializable {
+
+    private int route_id;
+
+    private String route_name;
+
+    private String route_description;
+
+    private int r_id;
+
+    public int getRoute_id() {
+        return route_id;
+    }
+
+    public void setRoute_id(int route_id) {
+        this.route_id = route_id;
+    }
+
+    public String getRoute_name() {
+        return route_name;
+    }
+
+    public void setRoute_name(String route_name) {
+        this.route_name = route_name;
+    }
+
+    public String getRoute_description() {
+        return route_description;
+    }
+
+    public void setRoute_description(String route_description) {
+        this.route_description = route_description;
+    }
+
+    public int getR_id() {
+        return r_id;
+    }
+
+    public void setR_id(int r_id) {
+        this.r_id = r_id;
+    }
+
+}
